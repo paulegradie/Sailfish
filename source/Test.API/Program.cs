6accf8b113791781e9f67b6841c82623864cb9f8
Paul Gradie <paul.e.gradie@gmail.com>
2022-06-05 23:51:17 +1000

    delete unused project

--- source/Test.API/Program.cs
+++ source/Test.API/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.Add
 
 var app = builder.Build();
 
