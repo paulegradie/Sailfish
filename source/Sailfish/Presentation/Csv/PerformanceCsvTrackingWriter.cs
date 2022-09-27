caaf2a2f4a4e2cbc26d96ae41d30865bb9df0e5b
Paul <paul.e.gradie@gmail.com>
2022-09-27 14:44:36 +1000

    do not write empty results

--- source/Sailfish/Presentation/Csv/PerformanceCsvTrackingWriter.cs
+++ source/Sailfish/Presentation/Csv/PerformanceCsvTrackingWriter.cs
@@ -6,33 +6,24 @@ using System.Threading.Tasks;
 using CsvHelper;
 using Sailfish.Contracts.Public.CsvMaps;
 using Sailfish.Statistics;
-using Sailfish.Utils;
 
 namespace Sailfish.Presentation.Csv;
 
 internal class PerformanceCsvTrackingWriter : IPerformanceCsvTrackingWriter
 {
-    private readonly IFileIo fileIo;
-
-    public PerformanceCsvTrackingWriter(IFileIo fileIo)
-    {
-        this.fileIo = fileIo;
-    }
-
     public async Task<string> ConvertToCsvStringContent(List<ExecutionSummary> result)
     {
         var filePath = Path.GetTempFileName();
-        using (var writer = new StreamWriter(filePath))
-        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        await using (var writer = new StreamWriter(filePath))
+        await using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
         {
             csv.Context.RegisterClassMap<TestCaseDescriptiveStatisticsMap>();
 
-            foreach (var container in result)
+            foreach (var records in result.Select(container => container
+                         .CompiledResults
+                         .Select(x => x.DescriptiveStatistics)
+                         .Where(x => x is not null)))
             {
-                var records = container
-                    .CompiledResults
-                    .Select(x => x.DescriptiveStatistics);
-
                 await csv.WriteRecordsAsync(records);
             }
         }
