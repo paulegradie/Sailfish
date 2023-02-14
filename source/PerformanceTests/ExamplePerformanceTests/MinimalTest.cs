3cbe7f4f221228b4fd04ec0b2f1c3edd40d47f61
Paul <paul.e.gradie@gmail.com>
2023-02-15 08:10:53 +1100

    use max to prevent -1 when no variables are present

---
+++ source/PerformanceTests/ExamplePerformanceTests/MinimalTest.cs
@@ -0,0 +1,12 @@
+﻿using Sailfish.Attributes;
+
+namespace PerformanceTests.ExamplePerformanceTests;
+
+[Sailfish]
+public class MinimalTest
+{
+    [SailfishMethod]
+    public void Minimal()
+    {
+    }
+}
