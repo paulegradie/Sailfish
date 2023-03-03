efc8eeac9065bca30cae3ab959481b51410646b5
Paul <paul.e.gradie@gmail.com>
2023-03-03 12:48:28 +1100

    remove unused usings

--- source/Sailfish/RunSettingsBuilder.cs
+++ source/Sailfish/RunSettingsBuilder.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using Accord.Collections;
 using Sailfish.Analysis;
 using Sailfish.Presentation;
+
 
 namespace Sailfish;
 
