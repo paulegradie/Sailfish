fb6b8d02167aaef700a1cf75394bab98fd27df4f
Paul <paul.e.gradie@gmail.com>
2023-03-26 18:34:30 +1100

    rewrite line-finder to only find in the right class

---
+++ source/PerformanceTests/ExamplePerformanceTests/Discoverable/WeirdBehaviorTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using Sailfish.Attributes;
+
+namespace PerformanceTests.ExamplePerformanceTests.Discoverable;
+
+[Sailfish(NumIterations = 6)]
+public class ExampleComponentTest
+{
+    public const string A = "Wow";
+    public const string B = "SoFast";
+
+    [SailfishVariable(10000, 100000, 1000000)]
+    public int N { get; set; }
+
+    [SailfishMethod]
+    public void Interpolate()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            ExampleOctopusComponent.InterpolateStrings(A, B);
+        }
+    }
+
+    [SailfishMethod]
+    public void Concatenate()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            ExampleOctopusComponent.ConcatenateStrings(A, B);
+        }
+    }
+
+    [SailfishMethod]
+    public void StringBuilder()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            ExampleOctopusComponent.StringBuilder(A, B);
+        }
+    }
+}
+
+public class ExampleOctopusComponent
+{
+    public static string InterpolateStrings(string a, string b)
+    {
+        return $"{a}-{b}";
+    }
+
+    public static string ConcatenateStrings(string a, string b)
+    {
+        return a + "-" + b;
+    }
+
+    public static string StringBuilder(string a, string b)
+    {
+        var builder = new StringBuilder();
+        return builder.Append(a).Append('-').Append(b).ToString();
+    }
+}
