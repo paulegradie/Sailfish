70a37b3ca6ab65f709cf9708bcc72fc584c2f1aa
Paul <paul.e.gradie@gmail.com>
2023-05-14 01:09:52 +1000

    update packagejson for deployment

--- site/next.config.js
+++ site/next.config.js
@@ -2,6 +2,7 @@ const withMarkdoc = require('@markdoc/next.js')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/github-pages',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
