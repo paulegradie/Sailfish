32edd50faa066536991c608484dfef51c39c4f24
Paul <paul.e.gradie@gmail.com>
2023-05-14 14:09:14 +1000

    cleanup

--- site/next.config.js
+++ site/next.config.js
@@ -1,7 +1,12 @@
 const withMarkdoc = require('@markdoc/next.js')
 
+const isProd = process.env.NODE_ENV === 'production'
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: isProd ? "/Sailfish" : "",
+  assetPrefix: isProd ? "/Sailfish" : "",
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
