4301d17cadd577009bc7ca323b5e64b33741f18a
Paul Gradie <paul.e.gradie@gmail.com>
2023-08-27 15:59:32 +1000

    improved prediction demo

--- source/ModelPredictions/Program.cs
+++ source/ModelPredictions/Program.cs
@@ -1,30 +1,34 @@
 ﻿using Sailfish.Analysis.Scalefish;
 
-const string rootDir = @"C:\Users\paule\code\Sailfish\source\PerformanceTests\bin\Release\net6.0\SailfishTestOutput";
-var file = Directory.GetFiles(rootDir, "ScalefishModels*").LastOrDefault();
+Console.WriteLine("This demo shows you how to load a model file and then use it to make predictions on scale.");
+Console.WriteLine("This is V1, so over the next number of iterations, we'll add more tooling around processing these models to make it easier to make predictions.");
 
-if (file is null) throw new Exception("Run a Sailfish test with a variable with multiple values and enable complexity for that variable to produce a Scalefish model file");
+var (cm, mm, pm) = GetBestFitModelForFirstMethod();
+Console.Clear();
+Console.WriteLine($"\nModel for : {cm.TestClassName}.{mm.TestMethodName}");
+Console.WriteLine($"With respect to {pm.PropertyName.Split(".").Last()}\n");
 
-var models = ModelLoader.LoadModelFile(file);
+Console.WriteLine("Model Details:\n");
+var function = pm.ComplexityResult.ComplexityFunction;
+Console.WriteLine($"Type:    {function.Name} - thats {function.OName}...");
+Console.WriteLine($"Quality: {function.Quality}\n");
 
-var someComplexityModel = models.First();
-Console.WriteLine("Models for class: " + someComplexityModel.TestClassName);
-
-var modelForSomeMethodInTheClass = someComplexityModel.TestMethodComplexityResults.First();
-Console.WriteLine("Models for method: " + modelForSomeMethodInTheClass.TestMethodName);
-
-var bestFitModelForMethod = modelForSomeMethodInTheClass.TestPropertyComplexityResults.First();
-Console.WriteLine("Model with respect to: " + bestFitModelForMethod.PropertyName);
-var nValues = Enumerable.Range(1, 100).ToList();
-
-
-Console.WriteLine(
-    $"Predictions for {someComplexityModel.TestClassName}.{modelForSomeMethodInTheClass.TestMethodName}.{bestFitModelForMethod.PropertyName}:");
-
+var nValues = new List<int>() { 1, 10, 100, 1000, 50_000 };
 foreach (var val in nValues)
 {
-    var result = bestFitModelForMethod.ComplexityResult.ComplexityFunction.Predict(val);
+    var result = pm.ComplexityResult.ComplexityFunction.Predict(val);
     Console.WriteLine($"f({val}) = " + Math.Round(result, 3));
 }
 
-Console.WriteLine("AT 50K: " + Math.Round(bestFitModelForMethod.ComplexityResult.ComplexityFunction.Predict(50_000)));
+(TestClassComplexityResult, TestMethodComplexityResult, TestPropertyComplexityResult) GetBestFitModelForFirstMethod()
+{
+    const string rootDir = @"C:\Users\paule\code\Sailfish\source\PerformanceTests\bin\Release\net6.0\SailfishTestOutput";
+    var file = Directory.GetFiles(rootDir, "ScalefishModels*").LastOrDefault();
+    if (file is null) throw new Exception("Run a Sailfish test with a variable with multiple values and enable complexity for that variable to produce a Scalefish model file");
+
+    var models = ModelLoader.LoadModelFile(file);
+    var classModel = models.First();
+    var methodModel = classModel.TestMethodComplexityResults.First();
+    var propertyModel = methodModel.TestPropertyComplexityResults.First();
+    return ((TestClassComplexityResult, TestMethodComplexityResult, TestPropertyComplexityResult))(classModel, methodModel, propertyModel);
+}
