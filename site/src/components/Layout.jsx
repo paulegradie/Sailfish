64bbb9da6a84b66509d4563734f3e21a07f39880
Paul Gradie <paul.e.gradie@gmail.com>
2023-09-16 22:19:54 +1000

    fix home text color

--- site/src/components/Layout.jsx
+++ site/src/components/Layout.jsx
@@ -66,9 +66,9 @@ function GitHubIcon(props) {
 
 function HomeIcon(props) {
   return (
-    <>
+    <div className={props.className}>
       {"Home"}
-    </>
+    </div>
   );
 }
 
@@ -100,7 +100,7 @@ function Header({ navigation }) {
       </div>
       <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
         <Link href={`/`} className="relative z-10">
-          <HomeIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
+          <HomeIcon className="dark:text-white" />
         </Link>
         <ThemeSelector className="relative z-10" />
         <Link href="https://github.com/paulegradie/Sailfish" className="group" aria-label="GitHub">
