3b724291a53712178a8df3c83fdffce83843972d
Paul Gradie <paul.e.gradie@gmail.com>
2024-02-24 13:09:59 +1100

    use lower csharp analysis common version for compat w ef

--- source/Sailfish.Analyzers/DiagnosticAnalyzers/LifecycleMethods/LifecycleAttributeNames.cs
+++ source/Sailfish.Analyzers/DiagnosticAnalyzers/LifecycleMethods/LifecycleAttributeNames.cs
@@ -2,19 +2,14 @@ namespace Sailfish.Analyzers.DiagnosticAnalyzers.LifecycleMethods;
 
 public class LifecycleAttributes
 {
-    public static readonly string[] Names;
-
-    static LifecycleAttributes()
-    {
-        Names = new[]
-        {
-            "SailfishGlobalSetup",
-            "SailfishGlobalTeardown",
-            "SailfishMethodSetup",
-            "SailfishMethodTeardown",
-            "SailfishIterationSetup",
-            "SailfishIterationTeardown",
-            "SailfishMethod"
-        };
-    }
+    public static readonly string[] Names =
+    [
+        "SailfishGlobalSetup",
+        "SailfishGlobalTeardown",
+        "SailfishMethodSetup",
+        "SailfishMethodTeardown",
+        "SailfishIterationSetup",
+        "SailfishIterationTeardown",
+        "SailfishMethod"
+    ];
 }
