b698e67aea3f33f6b526f5f0be754dac91b89d9c
Paul Gradie <paul.e.gradie@gmail.com>
2024-03-07 21:32:21 +1100

    more builders

---
+++ source/Tests.Library/Utils/Builders/TestCaseVariablesBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Sailfish.Contracts.Public.Models;
+using System.Collections.Generic;
+
+namespace Tests.Library.Utils.Builders;
+
+public class TestCaseVariablesBuilder
+{
+    private readonly List<TestCaseVariable> variables = new();
+
+    public TestCaseVariablesBuilder AddVariable(TestCaseVariable variable)
+    {
+        variables.Add(variable);
+        return this;
+    }
+
+    public TestCaseVariablesBuilder AddVariable(string name, object value)
+    {
+        variables.Add(new TestCaseVariable(name, value));
+        return this;
+    }
+
+    public TestCaseVariables Build()
+    {
+        return new TestCaseVariables(variables);
+    }
+}
