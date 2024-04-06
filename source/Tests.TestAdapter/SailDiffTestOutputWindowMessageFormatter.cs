2f7be95c22dc29a70a14d9f42fffe9b6917fabcf
Paul Gradie <paul.e.gradie@gmail.com>
2024-04-06 15:35:48 +1100

    cover test output messages

---
+++ source/Tests.TestAdapter/SailDiffTestOutputWindowMessageFormatter.cs
@@ -0,0 +1,80 @@
+using Sailfish.Analysis.SailDiff;
+using Sailfish.Analysis.SailDiff.Statistics.Tests;
+using Sailfish.Contracts.Public;
+using Sailfish.Contracts.Public.Models;
+using Sailfish.TestAdapter.Display.TestOutputWindow;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using Tests.TestAdapter.Utils;
+using Xunit;
+
+namespace Tests.TestAdapter;
+
+public class SailDiffTestOutputWindowMessageFormatterTests
+{
+    [Fact]
+    public void OutputIsCreatedCorrectly()
+    {
+        var id1 = new TestCaseId($"abc.wow()");
+        var testResultWithOutlierAnalysis = new TestResultWithOutlierAnalysis(new StatisticalTestResult(
+            5.0,
+            6.0,
+            5.0,
+            5.0,
+            345,
+            0.001,
+            SailfishChangeDirection.NoChange,
+            3,
+            3,
+            new[] { 1.0, 2, 3 },
+            new[] { 9.0, 10, 11 },
+            new Dictionary<string, object>()), null, null);
+
+        var sailDiffResult = new SailDiffResult(id1, testResultWithOutlierAnalysis);
+        var ids = new TestIds(new[] { id1.DisplayName }, new[] { "Id2" });
+        var settings = new SailDiffSettings();
+
+        var formatter = new SailDiffTestOutputWindowMessageFormatter();
+        var outputResult = formatter.FormTestOutputWindowMessageForSailDiff(sailDiffResult, ids, settings);
+
+        // don't bother converting this to raw string
+        const string expected = @"Before Ids: abc.wow()
+After Ids: Id2
+Statistical Test
+----------------
+Test Used:       TwoSampleWilcoxonSignedRankTest
+PVal Threshold:  0.001
+PValue:          0.001
+Change:          No Change  (reason: 0.001 > 0.001)
+
+|             | Before (ms) | After (ms) | 
+| ---         | ---         | ---        | 
+| Mean        |           5 |          6 | 
+| Median      |           5 |          5 | 
+| Sample Size |           3 |          3 | 
+
+";
+        outputResult.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void OutputIsNotWrittenWhenThereIsATestFailure()
+    {
+        var id1 = new TestCaseId($"abc.wow()");
+        const string exceptionMessage = "Exception Encountered";
+        var testResultWithOutlierAnalysis = new TestResultWithOutlierAnalysis(new Exception(exceptionMessage));
+
+        var sailDiffResult = new SailDiffResult(id1, testResultWithOutlierAnalysis);
+        var ids = new TestIds(new[] { id1.DisplayName }, new[] { Some.RandomString() });
+        var settings = new SailDiffSettings();
+
+        var formatter = new SailDiffTestOutputWindowMessageFormatter();
+        var outputResult = formatter.FormTestOutputWindowMessageForSailDiff(sailDiffResult, ids, settings);
+
+        const string expected = @$"Statistical testing failed:
+{exceptionMessage}
+";
+        outputResult.ShouldBe(expected);
+    }
+}
