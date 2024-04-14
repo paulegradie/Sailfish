1f6442637ca517cf3ef271d4040b99f32af863e7
Paul Gradie <paul.e.gradie@gmail.com>
2024-04-14 16:24:54 +1000

    Exceptions coverage

---
+++ source/Tests.Library/Analysis/ScaleFish/ScaleFishModelFunctionTests.cs
@@ -0,0 +1,28 @@
+using Sailfish.Analysis.ScaleFish;
+using Sailfish.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace Tests.Library.Analysis.ScaleFish;
+
+public class ScaleFishModelFunctionTests
+{
+    [Fact]
+    public void PredictThrowsWhenFunctionParametersNotSet()
+    {
+        Should.Throw<SailfishModelException>(() => new TestFunction().Predict(10));
+    }
+
+    class TestFunction : ScaleFishModelFunction
+    {
+        public override string Name { get; set; } = "TestFunction";
+        public override string OName { get; set; } = "O(Test)";
+        public override string Quality { get; set; } = "Great";
+        public override string FunctionDef { get; set; } = "TestFunctionDef";
+
+        public override double Compute(double bias, double scale, double x)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ source/Tests.Library/Execution/TestCaseEnumeratorTests.cs
@@ -0,0 +1,65 @@
+using MediatR;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Sailfish.Contracts.Public.Models;
+using Sailfish.Execution;
+using Sailfish.Logging;
+using Sailfish.Presentation.Console;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Threading;
+using System.Threading.Tasks;
+using Tests.Common.Utils;
+using Xunit;
+
+namespace Tests.Library.Execution;
+
+public class TestCaseEnumerationTests
+{
+    [Fact]
+    public async Task ExceptionsOnTestCaseEnumerationAreHandled()
+    {
+        var logger = Substitute.For<ILogger>();
+        var consoleWriter = Substitute.For<IConsoleWriter>();
+        var iterator = Substitute.For<ITestCaseIterator>();
+        var printer = Substitute.For<ITestCaseCountPrinter>();
+        var mediator = Substitute.For<IMediator>();
+        var summaryCompiler = Substitute.For<IClassExecutionSummaryCompiler>();
+        var settings = Substitute.For<IRunSettings>();
+        var engine = new SailfishExecutionEngine(logger, consoleWriter, iterator, printer, mediator, summaryCompiler, settings);
+
+        var enumerator = Substitute.For<IEnumerator<TestInstanceContainer>>();
+        enumerator.MoveNext().Throws<Exception>();
+        var instanceContainer = TestInstanceContainer.CreateTestInstance(
+            new object(),
+            typeof(TestCaseEnumerationTests).GetMethods().First(),
+            [],
+            [],
+            false,
+            new ExecutionSettings());
+        enumerator.Current.Returns(instanceContainer);
+
+        var testCaseEnumerator = Substitute.For<IEnumerable<TestInstanceContainer>>();
+        testCaseEnumerator.GetEnumerator().Returns(enumerator);
+
+        var provider = Substitute.For<ITestInstanceContainerProvider>();
+        provider.Test.Returns(typeof(TestCaseEnumerationTests));
+        provider.ProvideNextTestCaseEnumeratorForClass().Returns(testCaseEnumerator);
+
+        var result = await engine.ActivateContainer(
+            1,
+            2,
+            provider,
+            MemoryCache.Default,
+            Some.RandomString(),
+            [],
+            CancellationToken.None);
+
+        result.Count.ShouldBe(1);
+        result.Single().IsSuccess.ShouldBeFalse();
+        result.Single().Exception.Message.ShouldBe("Failed to create test cases for Tests.Library.Execution.TestCaseEnumerationTests");
+    }
+}
