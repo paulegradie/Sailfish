776e822996837ff0fd9750af93cc0bb6bf2de858
Paul Gradie <paul.e.gradie@gmail.com>
2025-07-24 19:13:40 +1000

    Add closure test

---
+++ source/PerformanceTests/ExamplePerformanceTests/ClosureTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sailfish.Attributes;
+using System.Collections.Generic;
+
+namespace PerformanceTests.ExamplePerformanceTests;
+
+[WriteToMarkdown]
+[Sailfish(SampleSize = 3, Disabled = false, DisableOverheadEstimation = true)]
+public class ScenarioSystem
+{
+    private readonly Random random = new();
+
+    [SailfishVariable(0, 1, 2)] 
+    public int Index { get; set; }
+
+    private int Count { get; set; }
+
+    public Scenario Data { get; set; } = null!;
+
+    [SailfishMethodSetup]
+    public void Setup()
+    {
+        Count = 0;
+    }
+
+    [SailfishIterationSetup]
+    public void EachIterationDo()
+    {
+        Data = MyDataContainer.GetDataForScenarioIndex(Index);
+    }
+
+    [SailfishMethod]
+    public async Task Scenario() // token is injected when requested
+    {
+        await Task.Yield();
+        Console.WriteLine($"{Data.Name}");
+    }
+}
+
+public class MyDataContainer
+{
+    private static List<Scenario> ScenarioList { get; set; } = [];
+
+    static MyDataContainer()
+    {
+        ScenarioList.Add(new Scenario("MaxConn=12"));
+        ScenarioList.Add(new Scenario("B"));
+        ScenarioList.Add(new Scenario("C"));
+    }
+
+    public static Scenario GetDataForScenarioIndex(int scenarioIndex)
+    {
+        return ScenarioList[scenarioIndex];
+    }
+};
+
+public record Scenario(string Name);
