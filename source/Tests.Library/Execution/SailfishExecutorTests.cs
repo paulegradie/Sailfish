72bc084a7cae647dca86992e9f019986a4905059
Paul Gradie <paul.e.gradie@gmail.com>
2025-11-13 23:26:39 +1100

    Add coverage

--- source/Tests.Library/Execution/SailfishExecutorTests.cs
+++ source/Tests.Library/Execution/SailfishExecutorTests.cs
@@ -14,6 +14,8 @@ using Sailfish.Logging;
 using Sailfish.Presentation;
 using Shouldly;
 using Xunit;
+using System.Reflection;
+
 
 namespace Tests.Library.Execution;
 
@@ -190,6 +192,117 @@ public class SailfishExecutorTests
 
         // Assert
         await mediator.Received(1).Publish(Arg.Any<TestRunCompletedNotification>(), Arg.Any<CancellationToken>());
+
+        [Fact]
+        public async Task Run_WithSeedInArgs_RandomizesOrderAndLogs()
+        {
+            // Arrange
+            var t1 = typeof(object);
+            var t2 = typeof(string);
+            var t3 = typeof(int);
+            var testInitResult = TestInitializationResult.CreateSuccess(new[] { t1, t2, t3 });
+            var classExecutionSummary = new ClassExecutionSummary(t1, new ExecutionSettings(), new List<ICompiledTestCaseResult>());
+
+            testFilter.FilterAndValidate(Arg.Any<IEnumerable<Type>>(), Arg.Any<IEnumerable<string>>()).Returns(testInitResult);
+            sailFishTestExecutor.Execute(Arg.Any<IEnumerable<Type>>(), Arg.Any<CancellationToken>()).Returns(new List<TestClassResultGroup>());
+            classExecutionSummaryCompiler.CompileToSummaries(Arg.Any<IEnumerable<TestClassResultGroup>>())
+                .Returns(new List<IClassExecutionSummary> { classExecutionSummary }.AsEnumerable());
+
+            runSettings.TestNames.Returns(new List<string>());
+            runSettings.TestLocationAnchors.Returns(new List<Type>());
+            runSettings.Seed.Returns((int?)null);
+            var args = new Sailfish.Extensions.Types.OrderedDictionary();
+            args.Add("seed", "123");
+            runSettings.Args.Returns(args);
+            runSettings.RunSailDiff.Returns(false);
+            runSettings.RunScaleFish.Returns(false);
+
+            var executor = new SailfishExecutor(mediator, sailFishTestExecutor, testCollector, testFilter, classExecutionSummaryCompiler, executionSummaryWriter, sailDiff, scaleFish, runSettings, logger);
+
+            // Act
+            var result = await executor.Run(CancellationToken.None);
+
+            // Assert
+            result.IsValid.ShouldBeTrue();
+            logger.Received(1).Log(
+                LogLevel.Information,
+                Arg.Is<string>(s => s.Contains("Randomized test class execution order with seed")),
+                Arg.Is<object[]>(os => os.Length == 1 && os[0] is int i && i == 123));
+        }
+
+        [Theory]
+        [InlineData("seed")]
+        [InlineData("SEED")]
+        [InlineData("randomseed")]
+        [InlineData("RandomSeed")]
+        [InlineData("rng")]
+        [InlineData("RNG")]
+        public void TryParseSeed_ParsesKnownKeys(string key)
+        {
+            // Arrange
+            var args = new Sailfish.Extensions.Types.OrderedDictionary();
+            args.Add(key, "987");
+
+            var method = typeof(Sailfish.Execution.SailfishExecutor)
+                .GetMethod("TryParseSeed", BindingFlags.Static | BindingFlags.NonPublic);
+            method.ShouldNotBeNull();
+
+            // Act
+            var value = (int?)method!.Invoke(null, new object[] { args });
+
+            // Assert
+            value.ShouldBe(987);
+        }
+
+        [Fact]
+        public void TryParseSeed_ReturnsNull_WhenMissingOrInvalid()
+        {
+            var method = typeof(Sailfish.Execution.SailfishExecutor)
+                .GetMethod("TryParseSeed", BindingFlags.Static | BindingFlags.NonPublic);
+            method.ShouldNotBeNull();
+
+            // Missing key
+            var argsEmpty = new Sailfish.Extensions.Types.OrderedDictionary();
+            var resEmpty = (int?)method!.Invoke(null, new object[] { argsEmpty });
+            resEmpty.ShouldBeNull();
+
+            // Invalid value
+            var argsInvalid = new Sailfish.Extensions.Types.OrderedDictionary();
+            argsInvalid.Add("seed", "not-an-int");
+            var resInvalid = (int?)method!.Invoke(null, new object[] { argsInvalid });
+            resInvalid.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Run_WithTestDiscoveryErrors_LogsErrorSummaryAndDetails()
+        {
+            // Arrange
+            var errors = new Dictionary<string, List<string>>
+            {
+                { "Error reason", new List<string> { "TestName1", "TestName2" } }
+            };
+            var testInitResult = TestInitializationResult.CreateFailure(Array.Empty<Type>(), errors);
+
+            testFilter.FilterAndValidate(Arg.Any<IEnumerable<Type>>(), Arg.Any<IEnumerable<string>>()).Returns(testInitResult);
+            runSettings.TestNames.Returns(new List<string>());
+            runSettings.TestLocationAnchors.Returns(new List<Type>());
+
+            var executor = new SailfishExecutor(mediator, sailFishTestExecutor, testCollector, testFilter, classExecutionSummaryCompiler, executionSummaryWriter, sailDiff, scaleFish, runSettings, logger);
+
+            // Act
+            await executor.Run(CancellationToken.None);
+
+            // Assert summary line
+            logger.Received().Log(
+                LogLevel.Error,
+                Arg.Is<string>(s => s.Contains("errors encountered while discovering tests")),
+                Arg.Is<object[]>(os => os.Length == 1 && os[0] is int n && n == 2));
+
+            // Assert reason and individual names were logged
+            logger.Received().Log(LogLevel.Error, Arg.Is<string>(s => s.Contains("{Reason}")), Arg.Any<object[]>());
+            logger.Received().Log(LogLevel.Error, Arg.Is<string>(s => s.Contains("--- {TestName}")), Arg.Any<object[]>());
+        }
+
     }
 }
 
